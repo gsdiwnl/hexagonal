f5b4472b8ed4d62170b0e30e6d53688b14956cb3
Gabriel Dullius <dulliusgabriel@gmail.com>
2025-02-03 14:18:00 -0300

    spring template

---
+++ src/test/java/br/com/db/hexagonal/ApplicationTest.java
@@ -0,0 +1,13 @@
+package br.com.db.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
